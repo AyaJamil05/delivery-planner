53defc19e4f7cd33b618edc272b5224d69f0e86b
Aya Jamil <ayajamil@gmail.com>
2026-09-24 00:09:47 +0000

    Initialisation du backend Spring Boot

---
+++ src/main/java/com/aya/delivery_planner/DeliveryPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.aya.delivery_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aya/delivery_planner/DeliveryPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aya.delivery_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
